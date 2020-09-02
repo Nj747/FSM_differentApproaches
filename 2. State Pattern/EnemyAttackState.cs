4dbe3bb4e2f49d997404ec48286a405591b71f1b
Nahuel <narmando94@gmail.com>
2020-09-02 12:39:39 -0300

    Adding the state pattern example

---
+++ 2. State Pattern/EnemyAttackState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackState : EnemyBaseState
+{
+    public override void EnterState(EnemyController enemy)
+    {
+        // Use for Initialize state
+    }
+
+    public override void UpdateState(EnemyController enemy)
+    {
+        // Do something for this Attack State
+
+        // Condition for changing state
+        if (enemy.distanceToPlayer > enemy.aggroDistance)
+        {
+            enemy.TransitionToState(enemy.IdleState);
+        }
+    }
+}
---
+++ 2. State Pattern/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float distanceToPlayer;
+    public float aggroDistance = 10.0f;
+
+    private EnemyBaseState currentState;
+    public EnemyBaseState CurrentState
+    {
+        get { return currentState; }
+    }
+
+    public readonly EnemyIdleState IdleState = new EnemyIdleState();
+    public readonly EnemyAttackState AttackState = new EnemyAttackState();
+    // keep adding states
+
+    private void Start()
+    {
+        TransitionToState(IdleState);
+    }
+
+    public void TransitionToState(EnemyBaseState state)
+    {
+        currentState = state;
+        currentState.EnterState(this);
+    }
+
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+        currentState.UpdateState(this);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.25f);
+        Gizmos.DrawSphere(transform.position, aggroDistance);
+    }
+}
